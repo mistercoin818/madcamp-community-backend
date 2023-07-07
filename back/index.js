6feda2442c5d8b37da5f49e75a17c7c2937d43eb
mistercoin <mistercoin@naver.com>
2023-07-07 19:17:25 +0900

    initial commmit with template

---
+++ back/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.port || 8000;
+
+app.get("/", (req, res) => {
+  console.log("requested.");
+});
+
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+});
