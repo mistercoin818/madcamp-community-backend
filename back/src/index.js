78d9097eb8d2263cf430830b247c199745ac221d
mistercoin <mistercoin@naver.com>
2023-07-08 13:47:04 +0900

    front-back 연동 및 db 연결하는 부분

--- back/src/index.js
+++ back/src/index.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.port || 8000;
+const path = require('path')
+const mysql = require('mysql2/promise')
 
-app.get('/', (req, res) => {
-  console.log('requested.');
+const pool = mysql.createPool({
+  host:'localhost',
+  port:'3306',
+  user:'root',
+  password:'1234',
+  database:'new_schema'
 });
 
-app.listen(PORT, () => {
-  console.log(`running on port ${PORT}`);
+const getConn = async()=>{
+  return await pool.getConnection(async(conn)=>conn);
+}
+
+app.listen(8080, function(){
+  console.log('listening on 8080');
+});
+
+// app.use(express.json());
+// let cors = require('cors');
+// app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../../front/build')))
+
+app.get('/product', async(req, res)=>{
+  const conn = await getConn();
+  const query = 'SELECT school from new_schema.user where name="jinhyeon"';
+  let [rows, fields] = await conn.query(query, []);
+  conn.release;
+
+  res.send(rows)
+})
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../../front/build/index.html'))
+});
+
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname, '../../front/build/index.html'))
 });
